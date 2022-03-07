3185526f22be34f11c4daf78bec4aadc56a0b391
shivam123425 <shivam123425@gmail.com>
2022-03-07 23:22:49 +0530

    remove extra arg

--- packages/server/errors/not-found-error.ts
+++ packages/server/errors/not-found-error.ts
@@ -3,16 +3,11 @@ import { CustomError } from "./custom-error";
 export class NotFoundError extends CustomError {
   statusCode: number = 404;
 
-  constructor(errors: ValidationError[]) {
+  constructor() {
     super("Not found");
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
   serializeErrors(): { message: String; field?: string }[] {
     return [{ message: "Not found" }]
   }
-}
-
-interface ValidationError {
-  msg: any;
-  param: string;
 }
