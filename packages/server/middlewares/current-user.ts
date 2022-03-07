044c3369bbc89732275098441504d990eec81267
shivam123425 <shivam123425@gmail.com>
2022-03-07 23:21:02 +0530

    Pick JWT_TOKEN from env variables

--- packages/server/middlewares/current-user.ts
+++ packages/server/middlewares/current-user.ts
@@ -2,20 +2,23 @@ import { Request, Response, NextFunction } from "express";
 import { UserJWTPayload } from "server/types";
 import jwt from "jsonwebtoken";
 
-export const currentUser =
-  (JWT_TOKEN: string) => (req: Request, res: Response, next: NextFunction) => {
-    if (!req.session?.jwt || !JWT_TOKEN) {
-      return next();
-    }
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.session?.jwt || process.env.JWT_TOKEN) {
+    return next();
+  }
 
-    try {
-      const payload = jwt.verify(
-        req.session.jwt,
-        process.env.JWT_TOKEN!
-      ) as UserJWTPayload;
-      req.currentUser = payload;
-    } catch (error) {
-      console.error(error);
-    }
-    next();
-  };
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_TOKEN!
+    ) as UserJWTPayload;
+    req.currentUser = payload;
+  } catch (error) {
+    console.error(error);
+  }
+  next();
+};
